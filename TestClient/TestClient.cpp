56d07a41ad10b9e4b041533ff64b804d8d24f90b
Fredrik Orderud <fredrik.orderud@ge.com>
2018-12-20 14:13:01 +0100

    Add cout diagnostics.

--- TestClient/TestClient.cpp
+++ TestClient/TestClient.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "../TestComponent/TestComponent_h.h"
 #include "../TestComponent/ComSupport.hpp"
 
@@ -8,11 +9,15 @@ int main() {
     // create COM object (will live in a separate apartment)
     CComPtr<ISharedMem> obj;
     CHECK(obj.CoCreateInstance(L"TestComponent.TestSharedMem"));
+    std::cout << "Object created" << std::endl;
 
     const unsigned int idx = 0;
     const unsigned char set_val = 42;
     CHECK(obj->SetData(idx, set_val));
+    std::cout << "SetData called" << std::endl;
+
     unsigned char get_val = 0;
     CHECK(obj->GetData(idx, &get_val));
+    std::cout << "GetData called" << std::endl;
     assert(get_val == set_val);
 }
