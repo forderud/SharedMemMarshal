712add5e7dffb740ad715578b96c8cbea043ba78
Fredrik Orderud <fredrik.orderud@ge.com>
2018-12-20 13:28:31 +0100

    Make shared mem writable.

--- TestComponent/SharedMem.hpp
+++ TestComponent/SharedMem.hpp
@@ -53,12 +53,12 @@ struct SharedMem {
             if (!ptr)
                 CheckErrorAndThrow("MapViewOfFile failed");
         } else {
-            // open shared mem segment in read-only mode
-            handle = OpenFileMapping(FILE_MAP_READ, FALSE, segm_name.c_str());
+            // open existing shared mem segment
+            handle = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, segm_name.c_str());
             if (!handle)
                 CheckErrorAndThrow("CreateFileMapping failed");
 
-            ptr = static_cast<unsigned char*>(MapViewOfFile(handle, FILE_MAP_READ, 0, 0, size));
+            ptr = static_cast<unsigned char*>(MapViewOfFile(handle, FILE_MAP_ALL_ACCESS, 0, 0, size));
             assert((reinterpret_cast<uintptr_t>(ptr) % 0x1000) == 0); // ptr is at least 4k aligned, since this is the mem. page size (HW limitation)
             if (!ptr)
                 CheckErrorAndThrow("MapViewOfFile failed");
