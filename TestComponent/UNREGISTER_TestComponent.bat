078aac79da83d60c21fae935edbd5421dd6911c1
Fredrik Orderud <forderud@gmail.com>
2018-12-21 21:51:25 +0100

    Add unregistation script.

---
+++ TestComponent/UNREGISTER_TestComponent.bat
@@ -0,0 +1,23 @@
+@echo off
+echo NOTICE: Script MUST be run as Administrator.
+:: Errors from "reg" tool are muted to avoid flooding the build log with errors from already deleted registry entries.
+
+:: Fix issue with "Run as Administrator" current dir
+setlocal enableextensions
+cd /d "%~dp0"
+
+
+:: Remove almost all traces of TestComponent from registry
+for %%P in (32 64) do (
+  for %%R in (HKEY_LOCAL_MACHINE HKEY_CURRENT_USER) do (
+    :: TypeLib & AppID
+    reg delete "%%R\SOFTWARE\Classes\TypeLib\{8E42F33F-E5E3-46B4-8B39-0A03DCF338CC}"   /f /reg:%%P 2> NUL
+    reg delete "%%R\SOFTWARE\Classes\AppID\{CDD196FE-70ED-46F4-BED7-57615CB78F9B}"     /f /reg:%%P 2> NUL
+  
+    :: class & interface
+    reg delete "%%R\SOFTWARE\Classes\CLSID\{B7B84F73-8638-4EE5-A486-2C9455C0CD2B}"     /f /reg:%%P 2> NUL
+    reg delete "%%R\SOFTWARE\Classes\Interface\{54E049DB-C39C-4025-B1EF-2FC0E5246277}" /f /reg:%%P 2> NUL
+  )
+)
+
+::pause
