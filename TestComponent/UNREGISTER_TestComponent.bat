51f7fddbc1995e5d23b74f5ab321cd6405848223
Fredrik Orderud <fredrik.orderud@ge.com>
2018-12-31 00:59:22 +0100

    update unreg script

--- TestComponent/UNREGISTER_TestComponent.bat
+++ TestComponent/UNREGISTER_TestComponent.bat
@@ -17,6 +17,8 @@ for %%P in (32 64) do (
     :: class & interface
     reg delete "%%R\SOFTWARE\Classes\CLSID\{B7B84F73-8638-4EE5-A486-2C9455C0CD2B}"     /f /reg:%%P 2> NUL
     reg delete "%%R\SOFTWARE\Classes\Interface\{54E049DB-C39C-4025-B1EF-2FC0E5246277}" /f /reg:%%P 2> NUL
+    reg delete "%%R\SOFTWARE\Classes\CLSID\{C8767698-8297-4BE1-904E-FC25034851D2}"     /f /reg:%%P 2> NUL
+    reg delete "%%R\SOFTWARE\Classes\Interface\{7D278258-3B05-45D3-AD60-CF65497B56B6}" /f /reg:%%P 2> NUL
   )
 )
 
