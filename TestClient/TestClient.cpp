a18db25e5f1e88f43b496bf4375a5cf97265d5f3
Fredrik Orderud <fredrik.orderud@ge.com>
2018-12-31 01:29:33 +0100

    Marshal same IDataHandle object multiple times.

--- TestClient/TestClient.cpp
+++ TestClient/TestClient.cpp
@@ -11,17 +11,19 @@ int main() {
     CHECK(mgr.CoCreateInstance(L"TestComponent.DataCollection", nullptr, CLSCTX_LOCAL_SERVER));
     std::cout << "Collection created" << std::endl;
 
-    CComPtr<IDataHandle> obj;
-    CHECK(mgr->GetHandle(0, &obj));
-    std::cout << "Object retrieved" << std::endl;
+    for (size_t i = 0; i < 2; ++i) {
+        CComPtr<IDataHandle> obj;
+        CHECK(mgr->GetHandle(0, &obj));
+        std::cout << "Object retrieved" << std::endl;
 
-    const unsigned int idx = 0;
-    const unsigned char set_val = 42;
-    CHECK(obj->SetData(idx, set_val));
-    std::cout << "SetData called" << std::endl;
+        const unsigned int idx = 0;
+        const unsigned char set_val = 42;
+        CHECK(obj->SetData(idx, set_val));
+        std::cout << "SetData called" << std::endl;
 
-    unsigned char get_val = 0;
-    CHECK(obj->GetData(idx, &get_val));
-    std::cout << "GetData called" << std::endl;
-    assert(get_val == set_val);
+        unsigned char get_val = 0;
+        CHECK(obj->GetData(idx, &get_val));
+        std::cout << "GetData called" << std::endl;
+        assert(get_val == set_val);
+    }
 }
