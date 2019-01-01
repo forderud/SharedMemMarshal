e1beb121eb63aade5ea86e050c41336c27ede30a
Fredrik Orderud <forderud@gmail.com>
2019-01-01 11:38:53 +0100

    Rewrite tests to prepare for multithreaded testing.

--- TestClient/TestClient.cpp
+++ TestClient/TestClient.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <thread>
 #include "../TestComponent/TestComponent_h.h"
 #include "../TestComponent/ComSupport.hpp"
 
+
+void AccessOneHandle (ISharedMem * mgr, unsigned int idx) {
+    CComPtr<IDataHandle> obj;
+    CHECK(mgr->GetHandle(idx, &obj));
+    std::cout << "Object retrieved" << std::endl;
+
+    const unsigned int pos = 0;
+    const unsigned char set_val = 42;
+    CHECK(obj->SetData(pos, set_val));
+    std::cout << "SetData called" << std::endl;
+
+    unsigned char get_val = 0;
+    CHECK(obj->GetData(pos, &get_val));
+    std::cout << "GetData called" << std::endl;
+    assert(get_val == set_val);
+}
+
+void AccessTwoHandles (ISharedMem * mgr, unsigned int idx) {
+    CComPtr<IDataHandle> obj1;
+    CHECK(mgr->GetHandle(idx, &obj1));
+    std::cout << "Object #1 retrieved" << std::endl;
+
+    CComPtr<IDataHandle> obj2;
+    CHECK(mgr->GetHandle(idx, &obj2));
+    std::cout << "Object #2 retrieved" << std::endl;
+
+    const unsigned int pos = 0;
+    const unsigned char set_val = 42;
+    CHECK(obj1->SetData(pos, set_val));
+    std::cout << "SetData called" << std::endl;
+
+    unsigned char get_val = 0;
+    CHECK(obj2->GetData(pos, &get_val));
+    std::cout << "GetData called" << std::endl;
+    assert(get_val == set_val);
+}
 
 int main() {
     ComInitialize com(COINIT_MULTITHREADED);
@@ -12,22 +49,6 @@ int main() {
     std::cout << "Collection created" << std::endl;
 
     for (size_t i = 0; i < 2; ++i) {
-        CComPtr<IDataHandle> obj1;
-        CHECK(mgr->GetHandle(0, &obj1));
-        std::cout << "Object #1 retrieved" << std::endl;
-
-        CComPtr<IDataHandle> obj2;
-        CHECK(mgr->GetHandle(0, &obj2));
-        std::cout << "Object #2 retrieved" << std::endl;
-
-        const unsigned int idx = 0;
-        const unsigned char set_val = 42;
-        CHECK(obj1->SetData(idx, set_val));
-        std::cout << "SetData called" << std::endl;
-
-        unsigned char get_val = 0;
-        CHECK(obj2->GetData(idx, &get_val));
-        std::cout << "GetData called" << std::endl;
-        assert(get_val == set_val);
+        AccessTwoHandles(mgr, 0);
     }
 }
