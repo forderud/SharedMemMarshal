b7d1e2a47414368f0f6ec9541f7d6c79412055e9
Fredrik Orderud <forderud@gmail.com>
2019-01-02 10:12:33 +0100

    Only unregister withing HKEY_LOCAL_MACHINE and not HKEY_CURRENT_USER.

--- TestComponent/UNREGISTER_TestComponent.bat
+++ TestComponent/UNREGISTER_TestComponent.bat
@@ -9,22 +9,20 @@ cd /d "%~dp0"
 
 :: Remove almost all traces of TestComponent from registry
 for %%P in (32 64) do (
-  for %%R in (HKEY_LOCAL_MACHINE HKEY_CURRENT_USER) do (
-    :: TypeLib & AppID
-    reg delete "%%R\SOFTWARE\Classes\TypeLib\{8E42F33F-E5E3-46B4-8B39-0A03DCF338CC}"   /f /reg:%%P 2> NUL
-    reg delete "%%R\SOFTWARE\Classes\AppID\{CDD196FE-70ED-46F4-BED7-57615CB78F9B}"     /f /reg:%%P 2> NUL
-  
-    :: interfaces
-    reg delete "%%R\SOFTWARE\Classes\Interface\{54E049DB-C39C-4025-B1EF-2FC0E5246277}" /f /reg:%%P 2> NUL
-    reg delete "%%R\SOFTWARE\Classes\Interface\{7D278258-3B05-45D3-AD60-CF65497B56B6}" /f /reg:%%P 2> NUL
-    
-    :: DataCollection class
-    reg delete "%%R\SOFTWARE\Classes\TestComponent.DataCollection"                     /f          2> NUL
-    reg delete "%%R\SOFTWARE\Classes\TestComponent.DataCollection.1"                   /f          2> NUL
-    reg delete "%%R\SOFTWARE\Classes\CLSID\{C8767698-8297-4BE1-904E-FC25034851D2}"     /f /reg:%%P 2> NUL
-    :: DataHandle class
-    reg delete "%%R\SOFTWARE\Classes\CLSID\{B7B84F73-8638-4EE5-A486-2C9455C0CD2B}"     /f /reg:%%P 2> NUL
-  )
+  :: TypeLib & AppID
+  reg delete "HKCR\TypeLib\{8E42F33F-E5E3-46B4-8B39-0A03DCF338CC}"   /f /reg:%%P 2> NUL
+  reg delete "HKCR\AppID\{CDD196FE-70ED-46F4-BED7-57615CB78F9B}"     /f /reg:%%P 2> NUL
+
+  :: interfaces
+  reg delete "HKCR\Interface\{54E049DB-C39C-4025-B1EF-2FC0E5246277}" /f /reg:%%P 2> NUL
+  reg delete "HKCR\Interface\{7D278258-3B05-45D3-AD60-CF65497B56B6}" /f /reg:%%P 2> NUL
+
+  :: DataCollection class
+  reg delete "HKCR\TestComponent.DataCollection"                     /f          2> NUL
+  reg delete "HKCR\TestComponent.DataCollection.1"                   /f          2> NUL
+  reg delete "HKCR\CLSID\{C8767698-8297-4BE1-904E-FC25034851D2}"     /f /reg:%%P 2> NUL
+  :: DataHandle class
+  reg delete "HKCR\CLSID\{B7B84F73-8638-4EE5-A486-2C9455C0CD2B}"     /f /reg:%%P 2> NUL
 )
 
 ::pause
