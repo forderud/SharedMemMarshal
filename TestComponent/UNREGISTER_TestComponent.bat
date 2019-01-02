71099fbac5f8808490f1799347cc783e3fd684c7
Fredrik Orderud <forderud@gmail.com>
2019-01-02 10:34:01 +0100

    Stop unregistering TypeLib & AppID separately for 32 & 64bit.

--- TestComponent/UNREGISTER_TestComponent.bat
+++ TestComponent/UNREGISTER_TestComponent.bat
@@ -6,22 +6,20 @@ echo NOTICE: Script MUST be run as Administrator.
 setlocal enableextensions
 cd /d "%~dp0"
 
+:: remove joint 32/64bit registry enytries
+reg delete "HKCR\TypeLib\{8E42F33F-E5E3-46B4-8B39-0A03DCF338CC}" /f  2> NUL
+reg delete "HKCR\AppID\{CDD196FE-70ED-46F4-BED7-57615CB78F9B}"   /f  2> NUL
 
-:: Remove almost all traces of TestComponent from registry
 for %%P in (32 64) do (
-  :: TypeLib & AppID
-  reg delete "HKCR\TypeLib\{8E42F33F-E5E3-46B4-8B39-0A03DCF338CC}"   /f /reg:%%P 2> NUL
-  reg delete "HKCR\AppID\{CDD196FE-70ED-46F4-BED7-57615CB78F9B}"     /f /reg:%%P 2> NUL
-
-  :: interfaces
+  :: remove interfaces
   reg delete "HKCR\Interface\{54E049DB-C39C-4025-B1EF-2FC0E5246277}" /f /reg:%%P 2> NUL
   reg delete "HKCR\Interface\{7D278258-3B05-45D3-AD60-CF65497B56B6}" /f /reg:%%P 2> NUL
 
-  :: DataCollection class
+  :: remove DataCollection class
   reg delete "HKCR\TestComponent.DataCollection"                     /f          2> NUL
   reg delete "HKCR\TestComponent.DataCollection.1"                   /f          2> NUL
   reg delete "HKCR\CLSID\{C8767698-8297-4BE1-904E-FC25034851D2}"     /f /reg:%%P 2> NUL
-  :: DataHandle class
+  :: remove DataHandle class
   reg delete "HKCR\CLSID\{B7B84F73-8638-4EE5-A486-2C9455C0CD2B}"     /f /reg:%%P 2> NUL
 )
 
