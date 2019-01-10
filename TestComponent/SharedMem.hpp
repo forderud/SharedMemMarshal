0586c8d9e427ca7cad80b55a725c41e96400d3e7
Fredrik Orderud <fredrik.orderud@ge.com>
2019-01-10 10:26:23 +0100

    Improve error detection.

--- TestComponent/SharedMem.hpp
+++ TestComponent/SharedMem.hpp
@@ -22,7 +22,9 @@ template <class T> IStream& operator>> (IStream& stream, T& data) {
 static void CheckErrorAndThrow(const char * error_msg) {
     DWORD err = GetLastError();
 
-    if (err == ERROR_NOT_ENOUGH_MEMORY)
+    if (err == ERROR_FILE_NOT_FOUND)
+        throw std::runtime_error("ERROR_FILE_NOT_FOUND");
+    else if (err == ERROR_NOT_ENOUGH_MEMORY)
         throw std::runtime_error("ERROR_NOT_ENOUGH_MEMORY");
 
     throw std::runtime_error(error_msg); // default message
