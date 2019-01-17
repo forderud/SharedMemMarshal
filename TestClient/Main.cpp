eb30f6d8173487cd52237146b8e5e8ae8d69684a
Fredrik Orderud <fredrik.orderud@ge.com>
2019-01-17 18:04:54 +0100

    Remove unused ImpersonateUser class. Also, remove disabled "manual" class-loading.

--- TestClient/Main.cpp
+++ TestClient/Main.cpp
@@ -5,32 +5,6 @@
 #include "../TestComponent/ComSupport.hpp"
 
 
-/** RAII class for impersonating a different user. */
-class ImpersonateUser {
-public:
-    ImpersonateUser(std::wstring username, std::wstring password) {
-        const wchar_t domain[] = L""; // default domain
-        if (!LogonUser(username.c_str(), domain, password.c_str(), LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, &m_user_token.m_h)) {
-            auto err = GetLastError();
-            abort();
-        }
-        if (!ImpersonateLoggedOnUser(m_user_token)) {
-            auto err = GetLastError();
-            abort();
-        }
-    }
-    ~ImpersonateUser() {
-        if (m_user_token) {
-            if (!RevertToSelf()) {
-                auto err = GetLastError();
-                abort();
-            }
-        }
-    }
-private:
-    CHandle m_user_token;
-};
-
 static BYTE GetValue(IDataHandle & h, unsigned int idx) {
     BYTE * buffer = nullptr;
     unsigned int size = 0;
@@ -86,23 +60,12 @@ void AccessTwoHandles (ISharedMem * mgr, unsigned int idx) {
 
 int main() {
     ComInitialize com(COINIT_MULTITHREADED);
-    //ImpersonateUser impersonate(L"user", L"passwd");
 
     // create COM object in a separate process
     CComPtr<ISharedMem> mgr;
     {
-        const wchar_t progId[] = L"TestComponent.DataCollection";
-        DWORD class_context = CLSCTX_LOCAL_SERVER | CLSCTX_ENABLE_CLOAKING;
-#ifdef DEBUG_COM_ACTIVATION
-        // open Event Viewer, "Windows Logs" -> "System" log to see details on failures
-        CLSID clsid = {};
-        CHECK(CLSIDFromProgID(progId, &clsid));
-        CComPtr<IClassFactory> cf;
-        CHECK(CoGetClassObject(clsid, class_context, NULL, IID_IClassFactory, (void**)&cf));
-        CHECK(cf->CreateInstance(nullptr, IID_ISharedMem, (void**)&mgr));
-#else
-        CHECK(mgr.CoCreateInstance(progId, nullptr, class_context));
-#endif
+        DWORD class_context = CLSCTX_LOCAL_SERVER; // force-run in separate process
+        CHECK(mgr.CoCreateInstance(L"TestComponent.DataCollection", nullptr, class_context));
         std::cout << "Collection created" << std::endl;
     }
 
