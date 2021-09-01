15777b9d48b7e1a156980d24290ed31a43364820
Fredrik Orderud <fredrik.orderud@ge.com>
2021-09-01 16:22:47 +0200

    Simplify test.

--- TestClient/Main.cpp
+++ TestClient/Main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <thread>
 #include "../TestComponent/TestComponent_h.h"
 #include "../TestComponent/TestComponent_i.c"
 #include "../TestComponent/ComSupport.hpp"
@@ -23,21 +22,6 @@ static void SetValue(IDataHandle & h, unsigned int idx, BYTE val) {
     buffer[idx] = val;
 }
 
-
-void AccessOneHandle (ISharedMem * mgr) {
-    CComPtr<IDataHandle> obj;
-    CHECK(mgr->GetHandle(true, &obj));
-    std::cout << "Object retrieved" << std::endl;
-
-    const unsigned int pos = 0;
-    const unsigned char set_val = 42;
-    SetValue(*obj, pos, set_val);
-    std::cout << "SetData called" << std::endl;
-
-    unsigned char get_val = GetValue(*obj, pos);
-    std::cout << "GetData called" << std::endl;
-    assert(get_val == set_val);
-}
 
 void AccessTwoHandles (ISharedMem * mgr) {
     CComPtr<IDataHandle> obj1;
@@ -76,15 +60,7 @@ int main() {
         std::cout << "Collection created" << std::endl;
     }
 
-    {
-        // multithreaded testing (triggers simultaneous calls to MarshalInterface)
-        std::thread t1(AccessOneHandle, mgr);
-        std::thread t2(AccessOneHandle, mgr);
-        t1.join();
-        t2.join();
-    }
-
-    // single-threaded testing
+    // test shared-mem access
     for (size_t i = 0; i < 2; ++i) {
         AccessTwoHandles(mgr);
     }
