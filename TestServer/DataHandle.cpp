c0b8e96ce13e2f5f9634a1a58ba33e08a75c06be
Fredrik Orderud <fredrik.orderud@ge.com>
2021-09-05 14:09:17 +0200

    Move non-trivial impl. to cpp.

--- TestServer/DataHandle.cpp
+++ TestServer/DataHandle.cpp
@@ -1,3 +1,78 @@
 #include "DataHandle.hpp"
+#include "RefOwner.hpp"
+
 
 std::atomic<unsigned int> DataHandle::s_counter{ 0 };
+
+
+DataHandle::DataHandle() {
+    s_counter++;
+
+    // log object count to Visual Studio "Output" window
+    auto msg = std::string("DataHandle ctor. (") + std::to_string(s_counter) + " objects).\n";
+    OutputDebugString(msg.c_str());
+
+}
+
+DataHandle::~DataHandle() {
+    s_counter--;
+
+    // log object count to Visual Studio "Output" window
+    auto msg = std::string("DataHandle dtor. (") + std::to_string(s_counter) + " objects).\n";
+    OutputDebugString(msg.c_str());
+}
+
+void DataHandle::Initialize(BOOL writable) {
+    // create shared-mem segment
+    m_data.reset(new SharedMem(SharedMem::OWNER, "TestSharedMem", writable, 1024));
+}
+
+HRESULT DataHandle::GetData(/*out*/BYTE** buffer, /*out*/unsigned int* size) {
+    if (!buffer || !size)
+        return E_INVALIDARG;
+
+    *buffer = m_data->ptr;
+    *size = m_data->size;
+    return S_OK;
+}
+
+/** IMarshal implementation. Called from server (stub). */
+HRESULT DataHandle::GetUnmarshalClass(const IID& iid, void* pv, DWORD destContext, void* reserved, DWORD mshlFlags, CLSID* clsid) {
+    assert(iid == IID_IDataHandle);
+    assert(mshlFlags == MSHLFLAGS_NORMAL); mshlFlags; // normal out-of-process marshaling
+
+    *clsid = CLSID_DataHandleProxy; // use DataHandleProxy class for unmarshaling
+    return S_OK;
+}
+
+/** Indicate the total size of the marshaled object reference. Called from server (stub). */
+HRESULT DataHandle::GetMarshalSizeMax(const IID& iid, void* /*pv*/, DWORD /*destContext*/, void* /*reserved*/, DWORD mshlFlags, /*out*/ULONG* size) {
+    assert(iid == IID_IDataHandle);
+    assert(mshlFlags == MSHLFLAGS_NORMAL); mshlFlags; // normal out-of-process marshaling
+
+    constexpr ULONG OBJREF_STANDARD_SIZE = 68; // sizeof(OBJREF) with flags=OBJREF_STANDARD and empty resolver address
+    *size = sizeof(m_data->writable) + sizeof(m_data->size) + OBJREF_STANDARD_SIZE;
+    return S_OK;
+}
+
+/** Serialize object. Called from server (stub). */
+HRESULT DataHandle::MarshalInterface(IStream* strm, const IID& iid, void* pv, DWORD destContext, void* reserved, DWORD mshlFlags) {
+    // verify that comm is between processes on same computer with shared-mem support 
+    //if (destContext != MSHCTX_LOCAL)
+    //    return E_FAIL;
+
+    assert(iid == IID_IDataHandle);
+    assert(pv == this); pv;             // class marshals itself
+    assert(mshlFlags == MSHLFLAGS_NORMAL); mshlFlags; // normal out-of-process marshaling
+
+    // serialize shared-mem metadata
+    *strm << m_data->writable;
+    *strm << m_data->size;
+
+    // serialize reference to a RefOwner object to manage references to this object from the proxy
+    auto ref_owner = CreateLocalInstance<RefOwner>();
+    ref_owner->SetObject(static_cast<IDataHandle*>(this));
+    CHECK(CoMarshalInterface(strm, IID_IUnknown, ref_owner, MSHCTX_LOCAL, NULL, MSHLFLAGS_NORMAL));
+
+    return S_OK;
+}
--- TestServer/DataHandle.hpp
+++ TestServer/DataHandle.hpp
@@ -5,7 +5,6 @@
 #include "Resource.h"
 #include "TestServer.h"
 #include "SharedMem.hpp"
-#include "RefOwner.hpp"
 
 
 class ATL_NO_VTABLE DataHandle :
@@ -14,77 +13,22 @@ class ATL_NO_VTABLE DataHandle :
     public IDataHandle,
     public IMarshal {
 public:
-    DataHandle() {
-        s_counter++;
+    DataHandle();
 
-        // log object count to Visual Studio "Output" window
-        auto msg = std::string("DataHandle ctor. (") + std::to_string(s_counter) + " objects).\n";
-        OutputDebugString(msg.c_str());
+    /*NOT virtual*/ ~DataHandle();
 
-    }
+    void Initialize(BOOL writable);
 
-    /*NOT virtual*/ ~DataHandle() {
-        s_counter--;
-
-        // log object count to Visual Studio "Output" window
-        auto msg = std::string("DataHandle dtor. (") + std::to_string(s_counter) + " objects).\n";
-        OutputDebugString(msg.c_str());
-    }
-
-    void Initialize(BOOL writable) {
-        // create shared-mem segment
-        m_data.reset(new SharedMem(SharedMem::OWNER, "TestSharedMem", writable, 1024));
-    }
-
-    HRESULT GetData(/*out*/BYTE ** buffer, /*out*/unsigned int* size) override {
-        if (!buffer || !size)
-            return E_INVALIDARG;
-
-        *buffer = m_data->ptr;
-        *size = m_data->size;
-        return S_OK;
-    }
+    HRESULT GetData(/*out*/BYTE** buffer, /*out*/unsigned int* size) override;
 
     /** IMarshal implementation. Called from server (stub). */
-    HRESULT GetUnmarshalClass(const IID& iid, void * pv, DWORD destContext, void * reserved, DWORD mshlFlags, CLSID* clsid) override {
-        assert(iid == IID_IDataHandle);
-        assert(mshlFlags == MSHLFLAGS_NORMAL); mshlFlags; // normal out-of-process marshaling
-
-        *clsid = CLSID_DataHandleProxy; // use DataHandleProxy class for unmarshaling
-        return S_OK;
-    }
+    HRESULT GetUnmarshalClass(const IID& iid, void* pv, DWORD destContext, void* reserved, DWORD mshlFlags, CLSID* clsid) override;
 
     /** Indicate the total size of the marshaled object reference. Called from server (stub). */
-    HRESULT GetMarshalSizeMax(const IID& iid, void * /*pv*/, DWORD /*destContext*/, void * /*reserved*/, DWORD mshlFlags, /*out*/ULONG* size) override {
-        assert(iid == IID_IDataHandle);
-        assert(mshlFlags == MSHLFLAGS_NORMAL); mshlFlags; // normal out-of-process marshaling
-
-        constexpr ULONG OBJREF_STANDARD_SIZE = 68; // sizeof(OBJREF) with flags=OBJREF_STANDARD and empty resolver address
-        *size = sizeof(m_data->writable) + sizeof(m_data->size) + OBJREF_STANDARD_SIZE;
-        return S_OK;
-    }
+    HRESULT GetMarshalSizeMax(const IID& iid, void* /*pv*/, DWORD /*destContext*/, void* /*reserved*/, DWORD mshlFlags, /*out*/ULONG* size) override;
 
     /** Serialize object. Called from server (stub). */
-    HRESULT MarshalInterface(IStream* strm, const IID& iid, void * pv, DWORD destContext, void * reserved, DWORD mshlFlags) override {
-        // verify that comm is between processes on same computer with shared-mem support 
-        //if (destContext != MSHCTX_LOCAL)
-        //    return E_FAIL;
-
-        assert(iid == IID_IDataHandle);
-        assert(pv == this); pv;             // class marshals itself
-        assert(mshlFlags == MSHLFLAGS_NORMAL); mshlFlags; // normal out-of-process marshaling
-
-        // serialize shared-mem metadata
-        *strm << m_data->writable;
-        *strm << m_data->size;
-
-        // serialize reference to a RefOwner object to manage references to this object from the proxy
-        auto ref_owner = CreateLocalInstance<RefOwner>();
-        ref_owner->SetObject(static_cast<IDataHandle*>(this));
-        CHECK(CoMarshalInterface(strm, IID_IUnknown, ref_owner, MSHCTX_LOCAL, NULL, MSHLFLAGS_NORMAL));
-
-        return S_OK;
-    }
+    HRESULT MarshalInterface(IStream* strm, const IID& iid, void* pv, DWORD destContext, void* reserved, DWORD mshlFlags) override;
 
     /** Deserialize object. Called from client (proxy). */
     HRESULT UnmarshalInterface(IStream* strm, const IID& iid, void ** ppv) override {
