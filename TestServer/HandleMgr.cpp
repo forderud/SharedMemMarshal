de43e3e3b70a21c0e2c6273004f348a44112bbb8
Fredrik Orderud <fredrik.orderud@ge.com>
2021-09-05 14:12:45 +0200

    move impl. to cpp.

--- TestServer/HandleMgr.cpp
+++ TestServer/HandleMgr.cpp
@@ -1,3 +1,34 @@
 #include "HandleMgr.hpp"
+#include "DataHandle.hpp"
+
 
 std::atomic<unsigned int> HandleMgr::s_counter{ 0 };
+
+
+HandleMgr::HandleMgr() {
+    s_counter++;
+
+    // log object count to Visual Studio "Output" window
+    auto msg = std::string("HandleMgr ctor. (") + std::to_string(s_counter) + " objects).\n";
+    OutputDebugString(msg.c_str());
+}
+
+HandleMgr::~HandleMgr() {
+    s_counter--;
+
+    // log object count to Visual Studio "Output" window
+    auto msg = std::string("HandleMgr dtor. (") + std::to_string(s_counter) + " objects).\n";
+    OutputDebugString(msg.c_str());
+}
+
+HRESULT HandleMgr::GetHandle(BOOL writable, IDataHandle** object) {
+    // create object
+    auto obj1 = CreateLocalInstance<DataHandle>();
+    obj1->Initialize(writable);
+
+    // cast to IDataHandle and return to caller
+    CComPtr<IDataHandle> obj2;
+    CHECK(obj1.QueryInterface(&obj2));
+    *object = obj2.Detach();
+    return S_OK;
+}
--- TestServer/HandleMgr.hpp
+++ TestServer/HandleMgr.hpp
@@ -1,43 +1,20 @@
 #pragma once
+#include <memory>
 #include "ComSupport.hpp"
 #include "Resource.h"
 #include "TestServer.h"
-#include "DataHandle.hpp"
 
 
 class ATL_NO_VTABLE HandleMgr :
     public CComObjectRootEx<CComMultiThreadModel>, // also compatible with single-threaded apartment
     public CComCoClass<HandleMgr, &CLSID_HandleMgr>,
-    public IHandleMgr
-{
+    public IHandleMgr {
 public:
-    HandleMgr(){
-        s_counter++;
+    HandleMgr();
 
-        // log object count to Visual Studio "Output" window
-        auto msg = std::string("HandleMgr ctor. (") + std::to_string(s_counter) + " objects).\n";
-        OutputDebugString(msg.c_str());
-    }
+    /*NOT virtual*/ ~HandleMgr();
 
-    /*NOT virtual*/ ~HandleMgr() {
-        s_counter--;
-
-        // log object count to Visual Studio "Output" window
-        auto msg = std::string("HandleMgr dtor. (") + std::to_string(s_counter) + " objects).\n";
-        OutputDebugString(msg.c_str());
-    }
-
-    HRESULT GetHandle(BOOL writable, IDataHandle ** object) override {
-        // create object
-        auto obj1 = CreateLocalInstance<DataHandle>();
-        obj1->Initialize(writable);
-
-        // cast to IDataHandle and return to caller
-        CComPtr<IDataHandle> obj2;
-        CHECK(obj1.QueryInterface(&obj2));
-        *object = obj2.Detach();
-        return S_OK;
-    }
+    HRESULT GetHandle(BOOL writable, IDataHandle** object) override;
 
     DECLARE_REGISTRY_RESOURCEID(IDR_HandleMgr)
 
