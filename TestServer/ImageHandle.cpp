cbe6fd0f9ef49f72faf3e39c3ed09777b1048189
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-20 18:58:32 +0100

    Start working on a new ImageHandle class.

---
+++ TestServer/ImageHandle.cpp
@@ -0,0 +1,84 @@
+#include "ImageHandle.hpp"
+#include "RefOwner.hpp"
+
+
+ImageHandle::ImageHandle() {
+}
+
+ImageHandle::~ImageHandle() {
+}
+
+void ImageHandle::Initialize(BOOL writable) {
+    // create shared-mem segment
+    m_data.reset(new SharedMem(SharedMem::OWNER, L"SharedMemMarshal.ImageHandle", writable, 1024*1024));
+}
+
+HRESULT ImageHandle::GetRawData(/*out*/BYTE** buffer, /*out*/unsigned int* size) {
+    if (!buffer || !size)
+        return E_INVALIDARG;
+
+    *buffer = m_data->ptr;
+    *size = m_data->size;
+    return S_OK;
+}
+
+/** IMarshal implementation. Called from server (stub). */
+HRESULT ImageHandle::GetUnmarshalClass(const IID& iid, void* pv, DWORD destContext, void* reserved, DWORD mshlFlags, CLSID* clsid) {
+    assert(iid == IID_IDataHandle);
+    assert(mshlFlags == MSHLFLAGS_NORMAL); mshlFlags; // normal out-of-process marshaling
+
+    *clsid = CLSID_DataHandleProxy; // use DataHandleProxy class for unmarshaling
+    return S_OK;
+}
+
+/** Indicate the total size of the marshaled object reference. Called from server (stub). */
+HRESULT ImageHandle::GetMarshalSizeMax(const IID& iid, void* /*pv*/, DWORD /*destContext*/, void* /*reserved*/, DWORD mshlFlags, /*out*/ULONG* size) {
+    assert(iid == IID_IDataHandle);
+    assert(mshlFlags == MSHLFLAGS_NORMAL); mshlFlags; // normal out-of-process marshaling
+
+    constexpr ULONG OBJREF_STANDARD_SIZE = 68; // sizeof(OBJREF) with flags=OBJREF_STANDARD and empty resolver address
+    *size = sizeof(m_data->writable) + sizeof(m_data->size) + OBJREF_STANDARD_SIZE;
+    return S_OK;
+}
+
+/** Serialize object. Called from server (stub). */
+HRESULT ImageHandle::MarshalInterface(IStream* strm, const IID& iid, void* pv, DWORD destContext, void* reserved, DWORD mshlFlags) {
+    // verify that comm is between processes on same computer with shared-mem support 
+    //if (destContext != MSHCTX_LOCAL)
+    //    return E_FAIL;
+
+    assert(iid == IID_IDataHandle);
+    assert(pv == this); pv;             // class marshals itself
+    assert(mshlFlags == MSHLFLAGS_NORMAL); mshlFlags; // normal out-of-process marshaling
+
+    // serialize shared-mem metadata
+    RETURN_IF_FAILED(m_data->Serialize(*strm));
+
+    // serialize reference to a RefOwner object to manage references to this object from the proxy
+    auto ref_owner = CreateLocalInstance<RefOwner>();
+    ref_owner->SetObject(static_cast<IDataHandle*>(this));
+    RETURN_IF_FAILED(CoMarshalInterface(strm, IID_IUnknown, ref_owner, MSHCTX_LOCAL, NULL, mshlFlags));
+
+    return S_OK;
+}
+
+/** Deserialize object. Called from client (proxy). */
+HRESULT ImageHandle::UnmarshalInterface(IStream* strm, const IID& iid, void** ppv) {
+    abort(); // should never be called
+}
+
+/** Destroys a marshaled data packet. Have never been observed called. */
+HRESULT ImageHandle::ReleaseMarshalData(IStream* strm) {
+    // skip over shared-mem metadata
+    RETURN_IF_FAILED(strm->Seek({ SharedMem::MARSHAL_SIZE, 0 }, STREAM_SEEK_CUR, nullptr));
+
+    // release RefOwner ref-count
+    RETURN_IF_FAILED(CoReleaseMarshalData(strm));
+
+    return S_OK;
+}
+
+/** Releases all connections to an object. Have never been observed called.  */
+HRESULT ImageHandle::DisconnectObject(DWORD /*reserved*/) {
+    return E_UNEXPECTED;
+}
---
+++ TestServer/ImageHandle.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <memory>
+#include "ComSupport.hpp"
+#include "TestServer.h"
+#include "SharedMem.hpp"
+
+
+class ATL_NO_VTABLE ImageHandle :
+    public CComObjectRootEx<CComMultiThreadModel>, // also compatible with single-threaded apartment
+    public CComCoClass<ImageHandle>, // no registry entries
+    public IDataHandle,
+    public IMarshal {
+public:
+    ImageHandle();
+
+    /*NOT virtual*/ ~ImageHandle();
+
+    void Initialize(BOOL writable);
+
+    HRESULT GetRawData(/*out*/BYTE** buffer, /*out*/unsigned int* size) override;
+
+    /** IMarshal implementation. Called from server (stub). */
+    HRESULT GetUnmarshalClass(const IID& iid, void* pv, DWORD destContext, void* reserved, DWORD mshlFlags, CLSID* clsid) override;
+
+    /** Indicate the total size of the marshaled object reference. Called from server (stub). */
+    HRESULT GetMarshalSizeMax(const IID& iid, void* /*pv*/, DWORD /*destContext*/, void* /*reserved*/, DWORD mshlFlags, /*out*/ULONG* size) override;
+
+    /** Serialize object. Called from server (stub). */
+    HRESULT MarshalInterface(IStream* strm, const IID& iid, void* pv, DWORD destContext, void* reserved, DWORD mshlFlags) override;
+
+    /** Deserialize object. Called from client (proxy). */
+    HRESULT UnmarshalInterface(IStream* strm, const IID& iid, void** ppv) override;
+
+    /** Destroys a marshaled data packet. Have never been observed called. */
+    HRESULT ReleaseMarshalData(IStream* /*strm*/) override;
+
+    /** Releases all connections to an object. Have never been observed called.  */
+    HRESULT DisconnectObject(DWORD /*reserved*/) override;
+
+    BEGIN_COM_MAP(ImageHandle)
+        COM_INTERFACE_ENTRY(IDataHandle)
+        COM_INTERFACE_ENTRY(IMarshal)
+    END_COM_MAP()
+
+private:
+    std::unique_ptr<SharedMem>       m_data;
+};
