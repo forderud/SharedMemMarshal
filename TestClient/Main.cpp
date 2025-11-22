ded7c5973149df53295dcd81ec583d04bd8f038b
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-22 22:55:51 +0100

    .

--- TestClient/Main.cpp
+++ TestClient/Main.cpp
@@ -30,6 +30,8 @@ void AccessSharedMem (IHandleMgr& mgr) {
         CHECK(img->GetData(&frame));
 
         printf("Frame time=%f\n", frame.time);
+        printf("Frame dims={%u, %u}\n", frame.dims[0], frame.dims[1]);
+        printf("Frame size=%u\n", frame.size());
     }
 }
 
