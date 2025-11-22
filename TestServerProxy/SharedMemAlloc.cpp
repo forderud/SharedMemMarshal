d78ab32b2736e0d67c2f6215a888bc525c956bd4
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-22 20:34:14 +0100

    .

--- TestServerProxy/SharedMemAlloc.cpp
+++ TestServerProxy/SharedMemAlloc.cpp
@@ -1,5 +1,17 @@
 #include "SharedMemAlloc.hpp"
 
+
+static void CheckErrorAndThrow(const char* error_msg) {
+    DWORD err = GetLastError();
+
+    if (err == ERROR_FILE_NOT_FOUND)
+        throw std::runtime_error("ERROR_FILE_NOT_FOUND");
+    else if (err == ERROR_NOT_ENOUGH_MEMORY)
+        throw std::runtime_error("ERROR_NOT_ENOUGH_MEMORY");
+
+    throw std::runtime_error(error_msg); // default message
+}
+
 SharedMemAlloc::SharedMemAlloc(MODE mode, size_t segm_size) : size(segm_size) {
     std::wstring segm_name = L"SharedMemMarshal.Segment";
 
--- TestServerProxy/SharedMemAlloc.hpp
+++ TestServerProxy/SharedMemAlloc.hpp
@@ -5,17 +5,6 @@
 #include <Windows.h>
 
 
-static void CheckErrorAndThrow(const char* error_msg) {
-    DWORD err = GetLastError();
-
-    if (err == ERROR_FILE_NOT_FOUND)
-        throw std::runtime_error("ERROR_FILE_NOT_FOUND");
-    else if (err == ERROR_NOT_ENOUGH_MEMORY)
-        throw std::runtime_error("ERROR_NOT_ENOUGH_MEMORY");
-
-    throw std::runtime_error(error_msg); // default message
-}
-
 /** Serialize/deserialize an arbitrary POD type. */
 template <class T> HRESULT operator<< (IStream& stream, const T& data) {
     unsigned long bytes_written = 0;
