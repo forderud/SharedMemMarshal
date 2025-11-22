c1f807852f1895f0ccc9dfcb2b8435e2406e06b3
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-22 22:01:17 +0100

    .

--- TestServerProxy/MarshalImage.hpp
+++ TestServerProxy/MarshalImage.hpp
@@ -2,7 +2,7 @@
 
 struct MarshalImage : Image2d {
     static constexpr unsigned int MarshalSize() {
-        return sizeof(time) + sizeof(pix_size) + sizeof(dims) + sizeof(offset);
+        return sizeof(time) + sizeof(pix_size) + sizeof(dims) + sizeof(img_offset) + sizeof(img_size);
     }
 
     HRESULT Serialize(IStream* strm) {
@@ -10,7 +10,8 @@ struct MarshalImage : Image2d {
         RETURN_IF_FAILED(*strm << time);
         RETURN_IF_FAILED(*strm << pix_size);
         RETURN_IF_FAILED(*strm << dims);
-        RETURN_IF_FAILED(*strm << offset);
+        RETURN_IF_FAILED(*strm << img_offset);
+        RETURN_IF_FAILED(*strm << img_size);
         return S_OK;
     }
 
@@ -19,9 +20,11 @@ struct MarshalImage : Image2d {
         RETURN_IF_FAILED(*strm >> time);
         RETURN_IF_FAILED(*strm >> pix_size);
         RETURN_IF_FAILED(*strm >> dims);
-        RETURN_IF_FAILED(*strm >> offset);
+        RETURN_IF_FAILED(*strm >> img_offset);
+        RETURN_IF_FAILED(*strm >> img_size);
         return S_OK;
     }
 
-    size_t offset = 0; // shared-mem segment offset
+    size_t img_offset = 0; // shared-mem segment offset
+    size_t img_size = 0;
 };
