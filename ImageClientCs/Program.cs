f6a47a42b3e24edeb6c177195f585ddbd6f4d8c6
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-23 15:38:07 +0100

    .

--- ImageClientCs/Program.cs
+++ ImageClientCs/Program.cs
@@ -8,8 +8,9 @@ namespace ImageClientCs
         static void Main(string[] args)
         {
             // instantiate COM server
-            IHandleMgr mgr = new ImageSource.HandleMgr();
-            // equivalent to Activator.CreateInstance(Type.GetTypeFromCLSID(typeof(ImageSource.HandleMgr).GUID));
+            // IHandleMgr mgr = new ImageSource.HandleMgr();
+            var guid = Guid.Parse("{C8767698-8297-4BE1-904E-FC25034851D2}"); // HandleMgr CLSID
+            var mgr = (IHandleMgr)Activator.CreateInstance(Type.GetTypeFromCLSID(guid));
 
             IImageHandle handle = mgr.GetImageHandle();
 
