b19593830fab9734f7e96eb43cdcbb6232137bb9
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-23 15:07:56 +0100

    Start writing C# client code
    WARNING: Doesn't work yet.

--- ImageClientCs/Program.cs
+++ ImageClientCs/Program.cs
@@ -6,6 +6,15 @@ namespace ImageClientCs
     {
         static void Main(string[] args)
         {
+            using ImageSource;
+
+            // instantiate COM server
+            IHandleMgr mgr = new ImageSource.HandleMgr();
+            // equivalent to Activator.CreateInstance(Type.GetTypeFromCLSID(typeof(ImageSource.HandleMgr).GUID));
+
+            IImageHandle handle = mgr.GetImageHandle();
+
+            Image2d frame = handle.GetData();
         }
     }
 }
