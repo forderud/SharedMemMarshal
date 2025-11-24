1977744eed48a68e201cd66895c341615aea8372
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-11-24 11:53:46 +0100

    .

--- ImageClientCs/Program.cs
+++ ImageClientCs/Program.cs
@@ -5,23 +5,26 @@ namespace ImageClientCs
 {
     internal class Program
     {
-        static void AccessImageData(IHandleMgr mgr)
+        static void AccessImageData(IHandleMgr mgr, bool verbose)
         {
             IImageHandle handle = mgr.GetImageHandle();
 
             Image2d frame = handle.GetData();
 
-            Console.WriteLine($"Frame time={frame.time}");
-            Console.WriteLine($"Frame format={frame.format}");
-            Console.WriteLine($"Frame dims=({frame.dims[0]}, {frame.dims[1]})");
-            Console.WriteLine($"Frame size={frame.data.Length}");
-            Console.Write("Frame data (first 128bytes): ");
-            for (int i = 0; (i < frame.data.Length) && (i < 128); i++)
+            if (verbose)
             {
-                var elm = frame.data.GetValue(i);
-                Console.Write(elm + ", ");
+                Console.WriteLine($"Frame time={frame.time}");
+                Console.WriteLine($"Frame format={frame.format}");
+                Console.WriteLine($"Frame dims=({frame.dims[0]}, {frame.dims[1]})");
+                Console.WriteLine($"Frame size={frame.data.Length}");
+                Console.Write("Frame data (first 128bytes): ");
+                for (int i = 0; (i < frame.data.Length) && (i < 128); i++)
+                {
+                    var elm = frame.data.GetValue(i);
+                    Console.Write(elm + ", ");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
         }
 
 
@@ -30,11 +33,15 @@ namespace ImageClientCs
             // instantiate COM server (in separate ImageSource process)
             IHandleMgr mgr = new ImageSource.HandleMgr();
 
-            for (int it = 0; it < 10; it++)
+            // access image-data 100 times with console logging every 10th iteration
+            for (int it = 0; it < 100; it++)
             {
-                Console.WriteLine($"Iteration {it}...");
-                AccessImageData(mgr);
-                Console.WriteLine();
+                bool verbose = (it % 10) == 0;
+                if (verbose)
+                    Console.WriteLine($"Iteration {it}...");
+                AccessImageData(mgr, verbose);
+                if (verbose)
+                    Console.WriteLine();
             }
 
             // call GC to release all COM server references before exiting
